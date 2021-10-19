6ee6202846259ea8b17612ce30cb91f4db3709e0
Smikhalcv <Smikhalcv@yandex.ru>
2021-10-19 23:02:31 +0300

    made hashset in exercise3

---
+++ JsonXmlCollection/Exercise3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonXmlCollection
+{
+    class Exercise3
+    {
+        private HashSet<int> _setNumber = new HashSet<int>();
+
+        public void EnterNumber()
+        {
+            bool flag = true;
+            string numberStr;
+            int number;
+            while (flag)
+            {
+                Console.WriteLine("Введите число -");
+                numberStr = Console.ReadLine();
+                if (numberStr == "")
+                {
+                    flag = false;
+                    break;
+                }
+                try
+                {
+                    number = Convert.ToInt32(numberStr);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите целое число.");
+                    throw;
+                }
+                CheckNumberInSet(number);
+            }
+            
+        }
+
+        private void CheckNumberInSet(int number)
+        {
+            if (_setNumber.Contains(number))
+            {
+                Console.WriteLine($"Число {number} уже есть во множестве.");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} добавлено во множестве.");
+                _setNumber.Add(number);
+            }
+        }
+    }
+}
--- JsonXmlCollection/Program.cs
+++ JsonXmlCollection/Program.cs
@@ -4,15 +4,17 @@ namespace JsonXmlCollection
 {
     class Program
     {
-        private static Exercise1 numberList = new Exercise1();
-        private static Exercise2 PhoneNote = new Exercise2();
+        private static Exercise1 _numberList = new Exercise1();
+        private static Exercise2 _phoneNote = new Exercise2();
+        private static Exercise3 _set = new Exercise3();
         static void Main(string[] args)
         {
-            /*numberList.FillingList();
-            numberList.PrintList();
-            numberList.DeleteRangeValue();
-            numberList.PrintList();*/
-            PhoneNote.PhoneNoteManager();
+            /*            _numberList.FillingList();
+                        _numberList.PrintList();
+                        _numberList.DeleteRangeValue();
+                        _numberList.PrintList();
+                        _phoneNote.PhoneNoteManager();*/
+            _set.EnterNumber();
         }
 
 
